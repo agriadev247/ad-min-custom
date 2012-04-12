ea9013c3c0102f4b07a67ce8636d7597fce40699
agriadev247 <agriadev247@gmail.com>
2012-04-12 21:26:52 +0200

    Improved the toolbar menu remover code

--- functions.php
+++ functions.php
@@ -1,31 +1,40 @@
 <?php
+/**
+* AD-min Custom
+*
+* Customize your admin area (the dashboard) in WordPress, easily.
+* 
+* Please note: this is NOT a fully functional theme, but rather a functions.php template to use in your themes.
+* It helps you to remove the uneccessary parts (menus, submenus, links) from the dashboard and/or add new custom ones.
+* Just put it into your theme directory if you don't have a functions.php or append to yours if you already have one.
+* Then simply edit and comment out the uneccesary parts to customize it to your needs.
+* And finally: don't forget to replace the 'mytheme' strings with your theme name.
+* Tested on WordPress version 3.3.1
+* 
+* @package WordPress
+* @subpackage ad-min-custom
+* @author agriadesign
+*/
 
-// Remove things from the top admin bar
-function mytheme_admin_bar_render() {
+// Remove menus from the TOOLBAR (top admin bar)
+add_action('wp_before_admin_bar_render', 'mytheme_remove_menus');
+function mytheme_remove_menus() {
     global $wp_admin_bar;
-    //Remove the WordPress logo menu
-    $wp_admin_bar->remove_menu('wp-logo');
-    //Remove the "My Account" item
-    $wp_admin_bar->remove_menu('my-account');
-    //Remove the "Site Name" menu
-    $wp_admin_bar->remove_menu('site-name');
-    //Remove the "My Sites/[Site Name]" menu and all submenus
-    $wp_admin_bar->remove_menu('my-sites');
-    //Remove the Shortlink menu
-    $wp_admin_bar->remove_menu('get-shortlink');
-    //Remove the view link
-    $wp_admin_bar->remove_menu('view');
-    //Remove the edit link for posts and terms
-    $wp_admin_bar->remove_menu('edit');
-    //Remove te "Add New" menu
-    $wp_admin_bar->remove_menu('new-content');
-    //Remove edit comments link with awaiting moderation count bubble
-    $wp_admin_bar->remove_menu('comments');
-    //Remove the update link for theme/plugin/core updates
-    $wp_admin_bar->remove_menu('updates');
-    //Remove the search form.
-    $wp_admin_bar->remove_menu('search');
+    $items = array(
+        'wp-logo',          // Remove the WORDPRESS LOGO menu
+        'my-account',       // Remove the MY ACCOUNT item
+        'site-name',        // Remove the SITE NAME menu
+        'my-sites',         // Remove the MY SITES menu
+        'get-shortlink',    // Remove the SHORTLINK menu
+        'view',             // Remove the VIEW link
+        'edit',             // Remove the EDIT link for posts and terms
+        'new-content',      // Remove the ADD NEW menu
+        'comments',         // Remove the EDIT COMMENTS link
+        'updates',          // Remove the UPDATE link for theme/plugin/core updates
+        'search',           // Remove the SEARCH form
+    );
+    foreach ($items as $value) {
+        $wp_admin_bar->remove_menu($value);;
+    }
 }
-add_action( 'wp_before_admin_bar_render', 'mytheme_admin_bar_render' );
-
 ?>
